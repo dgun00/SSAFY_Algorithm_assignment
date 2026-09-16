b949ce3a4064898b8359c485dfaadf050e336e95
dgun <hdg20042@gmail.com>
2026-09-16 23:27:38 +0900

    ㅈㅏㄱ작ㅓㅂㅅ업ㄴ순ㅓ서ㅊㅜㅅㅜㅈ수ㅇ정

--- src/SWEA_1267_작업순서_D6.java
+++ src/SWEA_1267_작업순서_D6.java
@@ -1,58 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class SWEA_1267_작업순서_D6 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		
-		
-		for(int tc=1; tc<=2; tc++) {
+
+		for (int tc = 1; tc <=10; tc++) {
+			StringBuilder sb = new StringBuilder();
 			String[] V_E = br.readLine().split(" ");
-			
-			int V =Integer.parseInt(V_E[0]);
-			int E =Integer.parseInt(V_E[1]);
-			
+
+			int V = Integer.parseInt(V_E[0]);
+			int E = Integer.parseInt(V_E[1]);
+
 			// 0번 인덱스 버리기
-			List<Integer>[] incomingEdges = new ArrayList[V+1];
-			
-			for (int i = 0; i < V+1; i++) {
-			    incomingEdges[i] = new ArrayList<>();
+			List<Integer>[] inEdges = new ArrayList[V + 1];
+			List<Integer>[] outEdges = new ArrayList[V + 1];
+
+//			// 0번 열,행은 버림 
+//			boolean[][] edgeMatrix = new boolean[V+1][V+1];
+
+			for (int i = 0; i < V + 1; i++) {
+				inEdges[i] = new ArrayList<>();
+				outEdges[i] = new ArrayList<>();
 			}
-			
+
 			String[] edges = br.readLine().split(" ");
-			
-			
-			boolean[] done = new boolean[V];
+
 			Deque<Integer> que = new ArrayDeque<>();
-			
-			
-			for(int i=0; i<E*2; i+=2) {
+
+			for (int i = 0; i < E * 2; i += 2) {
 				int st = Integer.parseInt(edges[i]);
-				int fi = Integer.parseInt(edges[i+1]);
-				
+				int fi = Integer.parseInt(edges[i + 1]);
+//				
+//				edgeMatrix[st][fi] = true;
+//				
 				// 해당 정점에 들어오는 간선 저장
-				incomingEdges[fi].add(st);
-				
+				inEdges[fi].add(st);
+
+				// 해당 정점에서 나가는 간선 저장
+				outEdges[st].add(fi);
+
+			}
+			boolean[] done = new boolean[V + 1];
+
+			// 초기 작업 상태일때 inedge가 0인 작업 큐에 넣기
+			for (int i = 1; i <= V; i++) {
+				if (inEdges[i].size() == 0) {
+					que.offer(i);
+
+					// 작업 끝낫다는 표시
+					done[i] = true;
+				}
+			}
+
+			sb.append("#").append(tc);
+			
+			while (!que.isEmpty()) {
+				int curTask = que.poll();
+				sb.append(" ").append(curTask);
+
+				// 현재 큐에서 나온 작업이 뻗어나간 엣지 제거
+				for (int i = 0; i < outEdges[curTask].size(); i++) {
+					// 현재 큐에서 나온 작업이 뻗어나가는 작업 구함
+					int fi = outEdges[curTask].get(i);
+
+					inEdges[fi].remove((Integer) curTask);
+
+				}
+
+				// inEdge가 0인 작업 큐에 넣기
+				for (int i = 1; i <= V; i++) {
+					if (inEdges[i].size() == 0 && !done[i]) {
+						que.offer(i);
+						// 작업 끝낫다는 표시
+						done[i] = true;
+					}
+				}
+
 			}
 			
+			System.out.println(sb);
 			
-			
-			while()
-//			for (int i = 1; i < incomingEdges.length; i++) {
-//				System.out.print(i+":");
-//				for(int j=0; j<incomingEdges[i].size();j++) {
-//					System.out.print(incomingEdges[i].get(j)+" ");
-//				}
-//				System.out.println();
-//			}
-			
-			
-			
-		}
-		
-		
-	}
-}
+
+		} // end of testcase
+
+	}// end of main
+}// end of class
