ef7c53240fc7686f0ab049b492187e6343740041
dgun <hdg20042@gmail.com>
2026-09-17 00:36:00 +0900

    최대상금 임시저장

--- src/SWEA_1244_최대상금_D3.java
+++ src/SWEA_1244_최대상금_D3.java
@@ -4,47 +4,74 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 /*
  * 접근:
  * 
  * 걍 조합 다 구하고 parseInt 해서 최대값 구하셈
  * 
  */
-xxxx
+
 public class SWEA_1244_최대상금_D3 {
-	
+
 	static int N;
+	static char[] str;
+	static int maxVal;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int TC = Integer.parseInt(br.readLine());
-		
-		for(int tc =1; tc<=TC; tc++) {
+
+		for (int tc = 1; tc <= TC; tc++) {
 			String[] line = br.readLine().split(" ");
-			
-			String num = line[0];
-			
+
+			str = new char[line[0].length()];
+
+			for (int i = 0; i < line[0].length(); i++) {
+				str[i] = line[0].charAt(i);
+			}
+
 			N = Integer.parseInt(line[1]);
+
+			maxVal = Integer.MIN_VALUE;
+			getCombi(0);
 			
-			
-			getCombi()
-			
-			
-			
+			System.out.println("#"+tc+" "+maxVal);			
 		}
-		
-	}
+
+	}// end of main
 
 	public static void getCombi(int cnt) {
-		
-		if(cnt == N) {
+
+		if (cnt == N) {
+			String toStr = new String(str);
+			
+			int val = Integer.parseInt(toStr);
+			
+			maxVal = Math.max(maxVal, val);
+			
 			return;
 		}
-		
-		for(int i = 0; i<N; i++) {
-			getCombi
+
+		for (int i = 0; i < str.length - 1; i++) {
+			for (int j = i + 1; j < str.length; j++) {
+				swap(i, j);
+
+				getCombi(cnt + 1);
+
+				swap(i, j);
+			}
 		}
-		
+
 	}
-}
+
+	public static void swap(int a, int b) {
+		char tmp = 0;
+
+		tmp = str[a];
+		str[a] = str[b];
+		str[b] = tmp;
+	}
+}// end of class
