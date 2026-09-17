1ce9bebb05fda55d18463e3920be3393fffc417d
dgun <hdg20042@gmail.com>
2026-09-17 09:31:23 +0900

    최대 상금 수정

--- src/SWEA_1244_최대상금_D3.java
+++ src/SWEA_1244_최대상금_D3.java
@@ -18,7 +18,7 @@ public class SWEA_1244_최대상금_D3 {
 	static int N;
 	static char[] str;
 	static int maxVal;
-
+	static Set<String> visited;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -34,8 +34,12 @@ public class SWEA_1244_최대상금_D3 {
 			}
 
 			N = Integer.parseInt(line[1]);
-
 			maxVal = Integer.MIN_VALUE;
+			
+			// 메모제이션용 set
+			// => 같은 깊이에서, 같은 str이 나온다면 그 뒤는 똑같기 때문에 가지치기 해준다.
+			visited = new HashSet<>();
+			
 			getCombi(0);
 			
 			System.out.println("#"+tc+" "+maxVal);			
@@ -48,6 +52,7 @@ public class SWEA_1244_최대상금_D3 {
 		if (cnt == N) {
 			String toStr = new String(str);
 			
+			
 			int val = Integer.parseInt(toStr);
 			
 			maxVal = Math.max(maxVal, val);
@@ -58,9 +63,14 @@ public class SWEA_1244_최대상금_D3 {
 		for (int i = 0; i < str.length - 1; i++) {
 			for (int j = i + 1; j < str.length; j++) {
 				swap(i, j);
-
-				getCombi(cnt + 1);
-
+				
+				String curKey = cnt + new String(str);
+				
+				if(!visited.contains(curKey)) {
+					visited.add(curKey);
+					getCombi(cnt + 1);
+				}
+				
 				swap(i, j);
 			}
 		}
